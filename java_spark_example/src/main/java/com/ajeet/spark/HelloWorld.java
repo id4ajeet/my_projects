f713cfb9d7508f42e6c839bfae9c18aae8456130
Ajeet Kumar Singh <id4ajeet@gmail.com>
2020-09-06 15:21:04 +0300

    HelloWorld javaSpark Example

---
+++ java_spark_example/src/main/java/com/ajeet/spark/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.ajeet.spark;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+/**
+ * @author <a href="mailto:id4ajeet@gmail.com">Ajeet</a>
+ */
+public class HelloWorld {
+
+    /**
+     * Run and call http://localhost:4567/hello
+     *
+     * @param args input
+     */
+    public static void main(String[] args) {
+        controllers();
+    }
+
+    public static void controllers() {
+        get("/hello", (req, res) -> "Hello World");
+
+        post("/hello/:name", (req, res) -> "Hello " + req.params(":name") + " Body sent is " + req.body());
+    }
+}
---
+++ java_spark_example/src/test/java/com/ajeet/spark/HelloWorldTest.java
@@ -0,0 +1,100 @@
+package com.ajeet.spark;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.with;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * @author <a href="mailto:AjeetKumar.Singh1@britebill.com">Ajeet</a>
+ */
+public class HelloWorldTest {
+
+    @Before
+    public void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 4567;
+    }
+
+    @Test
+    public void testHelloWorld() {
+        HelloWorld.controllers();
+
+        when().request("GET", "/hello")
+            .then().statusCode(200);
+    }
+
+    @Test
+    public void testHelloWorldLog() {
+        HelloWorld.controllers();
+
+        given().log().all()
+            .when().request("GET", "/hello")
+            .then().log().all().statusCode(200);
+    }
+
+    @Test
+    public void testHelloWorldResponse() {
+        HelloWorld.controllers();
+
+        Response response = get("/hello");
+
+        Assert.assertNotNull(response);
+
+        ResponseBody body = response.body();
+        Assert.assertEquals("Hello World", body.asString());
+
+        long timeInMS = response.time();
+        Assert.assertTrue(timeInMS < 1000);
+    }
+
+    @Test
+    public void testHelloWorldAssert() {
+        HelloWorld.controllers();
+
+        get("/hello")
+            .then()
+            .assertThat()
+            .body(equalTo("Hello World"))
+            .and()
+            .assertThat()
+            .time(is(lessThan(1000L)));
+    }
+
+    @Test
+    public void testHelloWorldPost() {
+        HelloWorld.controllers();
+
+        Response response = with().body("Dummy Body")
+            .when()
+            .post("/hello/Ajeet");
+
+        response.then()
+            .log().all()
+            .statusCode(200);
+
+        Assert.assertEquals("Hello Ajeet Body sent is Dummy Body", response.body().asString());
+    }
+
+    @Test
+    public void testHelloWorldWrongUrl() {
+        HelloWorld.controllers();
+
+        get("/hello/Ajeet")
+            .then()
+            .log().ifError()
+            .statusCode(404);
+    }
+}
+
